e45a55acd054030990f1d7ac08c64b81f88792f4
Rowi-1 <rowi_1one@students.uonbi.ac.ke>
2024-04-25 02:02:54 +0300

    fullset

---
+++ hello.py
@@ -0,0 +1,7 @@
+# This Python script asks for the user's name and greets them.
+
+# Prompt "Hello! What is your name?" and store the input in a variable
+user_name = input("Hello! What is your name? ")
+
+# Display a greeting message using the user's input
+print("Nice to meet you, " + user_name + "!")
